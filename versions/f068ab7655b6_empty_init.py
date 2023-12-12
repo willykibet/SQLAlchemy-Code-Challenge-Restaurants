23fcfbec9aeccf88447cb2a9765889d337f16c83
willykibet <willykipchumba22@gmail.com>
2023-12-12 20:51:18 +0300

    nhbyuj

---
+++ versions/f068ab7655b6_empty_init.py
@@ -0,0 +1,26 @@
+"""Empty Init
+
+Revision ID: f068ab7655b6
+Revises: 
+Create Date: 2023-12-11 19:50:14.567547
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+
+
+revision = 'f068ab7655b6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
